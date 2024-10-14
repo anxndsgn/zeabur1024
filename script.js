12813c5c4c97b40fbb7fddedc419d21ac9b844cd
xin <anxanxx@gmail.com>
2024-10-14 15:48:42 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+import { promotions } from './promotion.js';
+
+function zeabur1024Promotion() {
+  const today = new Date(Date.now());
+
+  if (today.toLocaleDateString() == '2024/10/14') {
+    console.log(promotions);
+  }
+}
+
+zeabur1024Promotion();
+
+console.log(
+  '%cBig Text %cSmall Text',
+  'font-size: 30px; color: red;',
+  'font-size: 12px; color: green;'
+);
